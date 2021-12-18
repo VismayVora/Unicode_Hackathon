50597851f1cc8c573030d687d8a6ede331e25761
Vismay Vora <voravismay9@gmail.com>
2021-12-18 16:35:45 +0530

    Created custom user model with email as login field

--- bestdeal/accounts/models.py
+++ bestdeal/accounts/models.py
@@ -1,3 +1,57 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from rest_framework.authtoken.models import Token
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifier
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password instead of username.
+        """
+        if not email:
+            raise ValueError('The Email must be set')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    username=None
+
+    # extra fields
+    email = models.EmailField(("Email Address"),primary_key=True)
+    name = models.CharField()
+    is_client = models.BooleanField(default=False)
+    is_vendor = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS=[]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def token(self):
+        token = Token.objects.get(user=User.objects.get(self.id))
+        return token
