10c8127e8c3c61d9252bc5a2ea9c728e72563941
Prathambhoir55 <80087152+Prathambhoir55@users.noreply.github.com>
2021-12-18 19:14:28 +0530

    Created Register and login serializers

---
+++ bestdeal/accounts/serializers.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import *
+import re
+
+email_pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+class ClientRegisterSerializer(serializers.ModelSerializer):
+    password= serializers.CharField(max_length = 16, min_length = 8, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+
+    # To validate data received
+    def validate(self, attrs):
+        email = attrs.get('email', ' ')
+        if not email_pattern.match(email):
+            raise serializers.ValidationError('Please enter a valid email!')
+        return attrs
+
+    # To create a new client user
+    def create(self, validated_data):
+        validated_data['is_active'] = False
+        validated_data['is_client'] = True
+        return User.objects.create_user(**validated_data)    
+
+class LoginSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(max_length=32,min_length=8,write_only = True)
+    
+    class Meta:
+        model = User
+        fields = ['email','password']
+
+class VendorRegisterSerializer(serializers.ModelSerializer):
+    password= serializers.CharField(max_length = 16, min_length = 8, write_only=True)
+
+    class Meta:
+        model = Vendor
+        fields = ['email', 'name', 'phone_no', 'industry']
+
+    # To validate data received
+    def validate(self, attrs):
+        email = attrs.get('email', ' ')
+        if not email_pattern.match(email):
+            raise serializers.ValidationError('Please enter a valid email!')
+        return attrs
+
+    # To create a vendor user
+    def create(self, validated_data):
+        validated_data['is_active'] = False
+        validated_data['is_vendor'] = True
+        return User.objects.create_user(**validated_data)  
