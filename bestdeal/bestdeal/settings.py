d430a9ad917fd0fa171a5758eba9859600dbf88c
Vismay Vora <voravismay9@gmail.com>
2021-12-19 01:21:24 +0530

    Added procfile and updated settings.py

--- bestdeal/bestdeal/settings.py
+++ bestdeal/bestdeal/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ac^za%6r@@)jpqtts6*44p4-jp9iu=l+4_qhid2u*4l8&c08hw'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
