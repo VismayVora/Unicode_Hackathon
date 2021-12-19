f65ba3f57f0f1d380fbdf7e62e5c3e505afe87a7
Prathambhoir55 <80087152+Prathambhoir55@users.noreply.github.com>
2021-12-20 01:10:29 +0530

    Fixed a bug in Items APIview and added whatsapp send message function

--- bestdeal/request_quote/views.py
+++ bestdeal/request_quote/views.py
@@ -1,10 +1,12 @@
 from re import S
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render
 from rest_framework import viewsets,permissions
 from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from .models import RequirementsDoc,Item,Quote
 from .serializers import RequirementsDocSerializer,ItemSerializer,QuoteSerializer
+from .whatsapp import send_message
 
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,7 +15,6 @@ from rest_framework import status
 class RequirementsDocView(viewsets.ModelViewSet):
     queryset = RequirementsDoc.objects.all()
     serializer_class = RequirementsDocSerializer
-
 
     def get_queryset(self):
         return RequirementsDoc.objects.filter(owner = self.request.user)
@@ -32,8 +33,10 @@ class ItemsAPI(APIView):
 
 
 	def post(self, request, pk):
-		request.data['req_doc'] = RequirementsDoc.objects.get(id= pk).id
-		serializer = ItemSerializer(data= request.data)
+		req_doc = RequirementsDoc.objects.get(id= pk).id
+		for i in range(len(request.data)):
+			request.data[i]['req_doc'] = req_doc
+		serializer = ItemSerializer(data= request.data, many=True)
 		if not serializer.is_valid():
 			return Response(serializer.errors, status= status.HTTP_403_FORBIDDEN)
 		serializer.save()
---
+++ bestdeal/request_quote/whatsapp.py
@@ -0,0 +1,22 @@
+import requests
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+
+
+def send_message(request, user):
+    url = "https://api.wassenger.com/v1/messages"
+    current_site = get_current_site(request).domain
+    relative_link = reverse('items-api')
+    link = 'http://'+current_site+relative_link
+    payload = {
+        "phone": str(user.number),
+        "message": "Hello {user.name}, There is a new requirement posted on BEST DEAL which might interest you. Click on this link: {link}"
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Token": "c0e1717b3de1545647d396ea3d59000aa65429387b643e0a7d979729370f47b4936b795410d6fd73"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    print(response.text)
